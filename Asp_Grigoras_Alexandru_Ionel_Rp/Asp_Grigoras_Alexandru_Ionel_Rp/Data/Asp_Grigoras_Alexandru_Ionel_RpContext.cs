eb80f5f05ae5c0d229aeaef92c854106fad5ef68
nuyonu <nuyonu@gmail.com>
2020-05-06 14:15:09 +0300

    10

---
+++ Asp_Grigoras_Alexandru_Ionel_Rp/Asp_Grigoras_Alexandru_Ionel_Rp/Data/Asp_Grigoras_Alexandru_Ionel_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Grigoras_Alexandru_Ionel_Rp.Models;
+
+namespace Asp_Grigoras_Alexandru_Ionel_Rp.Data
+{
+    public class Asp_Grigoras_Alexandru_Ionel_RpContext : DbContext
+    {
+        public Asp_Grigoras_Alexandru_Ionel_RpContext (DbContextOptions<Asp_Grigoras_Alexandru_Ionel_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Grigoras_Alexandru_Ionel_Rp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ Asp_Grigoras_Alexandru_Ionel_Rp/Asp_Grigoras_Alexandru_Ionel_Rp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Asp_Grigoras_Alexandru_Ionel_Rp.Data;
+using Asp_Grigoras_Alexandru_Ionel_Rp.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Asp_Grigoras_Alexandru_Ionel_Rp
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Asp_Grigoras_Alexandru_Ionel_Rp.Data.Asp_Grigoras_Alexandru_Ionel_RpContext _context;
+
+        public IndexModel(Asp_Grigoras_Alexandru_Ionel_Rp.Data.Asp_Grigoras_Alexandru_Ionel_RpContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
+            var movies = from m in _context.Movie
+                         select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
+        }
+    }
+}
